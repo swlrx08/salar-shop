300635ddebbfe9ad322ede7118a4c85937e2c9a6
salar <127493852+swlrx08@users.noreply.github.com>
2024-06-23 01:17:01 +0330

    complete sign_up

--- userauths/forms.py
+++ userauths/forms.py
@@ -4,6 +4,35 @@ from userauths.models import User
 
 
 class UserRegisterForm(UserCreationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={"placeholder": "Username"}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={"placeholder": "Email"}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder": "Password"}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder": "Confirm Password"}))
+
     class Meta:
         model = User
         fields = ['username', 'email']
+
+        # widgets = {
+        #     'username': forms.TextInput(attrs={
+        #         'class': 'form-control',
+        #         'placeholder': 'Username',
+        #
+        #     }),
+        #     'email': forms.EmailField(attrs={
+        #         'class': 'form-control',
+        #         'placeholder': 'Email',
+        #
+        #     }),
+        #     'password1': forms.PasswordInput(attrs={
+        #         'class': 'form-control',
+        #         'placeholder': 'Password',
+        #
+        #     }),
+        #     'password2': forms.TextInput(attrs={
+        #         'class': 'form-control',
+        #         'placeholder': 'Confirm Password',
+        #
+        #     }),
+        #
+        # }
