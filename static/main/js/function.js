129b1fa543d66444c26db9e85aba21c68233a18f
salar <127493852+swlrx08@users.noreply.github.com>
2024-06-26 07:08:35 -0700

    add comment with Jquary
    add comment in DB and show it in page without loading the page and all this done with jQuery

--- static/main/js/function.js
+++ static/main/js/function.js
@@ -1,7 +1,16 @@
 console.log("working fine");
 
-$("#commentForm").submit(function(e) {
+const monthNames = ["Jan","Feb","Mar","April","May","June",
+    "July","Aug","Sept","Oct","Nov", "Dec"
+];
+
+$("#commentForm").submit(function (e) {
     e.preventDefault();
+
+    let dt = new Date();
+    let time = dt.getDate() + "" + monthNames[dt.getUTCMonth()]+ ", "+ dt.getFullYear()
+
+    let images = $('#myImg').attr('src')
 
     $.ajax({
         data: $(this).serialize(),
@@ -12,8 +21,42 @@ $("#commentForm").submit(function(e) {
 
         dataType: "json",
 
-        success: function (response){
+        success: function (res) {
             console.log("Comment Saved to DB...")
+
+
+            if (res.bool == true) {
+                $("#review-res").html("Review Added Successfully")
+                $(".hide-comment-form").hide()
+                $(".add-review").hide()
+
+                let _html ='<div class="single-comment justify-content-between d-flex mb-30">'
+                        _html +=  '<div class="user justify-content-between d-flex">'
+                        _html += '<div class="thumb text-center">'
+                        _html += '<img src="'+ images +'" alt=""/>'
+                        _html += '<a href="#" class="font-heading text-brand">'+ res.context.user +'</a>'
+                        _html += '</div>'
+
+                        _html += '<div class="desc">'
+                        _html += '<div class="d-flex justify-content-between mb-10">'
+                        _html += '<div class="d-flex align-items-center">'
+                        _html += '<span class="font-xs text-muted">'+ time +' </span>'
+                        _html += '</div>'
+
+                        for (let i =1; i<= res.context.rating; i++){
+                            _html += '<i class="fas fa-star text-warning"></i>'
+                        }
+
+                        _html += '</div>'
+                        _html += '<p class="mb-10">'+ res.context.review +'</p>'
+
+                        _html += '</div>'
+                        _html += '</div>'
+                        _html += '</div>'
+
+                        $(".comment-list").prepend(_html)
+
+                }
         }
     })
 })
